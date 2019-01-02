b68feaba61f2bdd94d1e8fe0bd75e008e5a62118
Rachel Black <rachblondon@gmail.com>
2019-01-02 22:07:43 +0100

    design tweaks

--- src/components/Game.js
+++ src/components/Game.js
@@ -6,15 +6,37 @@ import CatFood from "./CatFood";
 export default class Game extends Component {
   render() {
     return (
-      <div style={{ position: "relative", color : "#484848" }}>
-        <div style={{ position: "absolute", zIndex: "1", margin: "2rem 1rem", fontFamily : 'VT323' }}>
-        <h3>Hungry : {this.props.gameDemoStats.hungry ? 'Yes! 😾' : "I'm full 😸"} </h3>
+      <div style={{ position: "relative" }}>
+        <div
+          style={{
+            position: "absolute",
+            zIndex: "1",
+            margin: "2rem 1rem",
+            fontFamily: "VT323",
+            backgroundColor: "white",
+            padding: "1rem",
+            opacity: "0.7",
+            borderRadius: ".25rem",
+            color: "black"
+          }}
+        >
+          <h3>
+            Hungry :{" "}
+            {this.props.gameDemoStats.hungry ? "Yes! 😾" : "I'm full 😸"}{" "}
+          </h3>
 
           <h3>Food stock: {this.props.gameDemoStats.foodStock}</h3>
           <h3>Age: {this.props.gameDemoStats.age}</h3>
           {/* <h3>Plays: {this.props.gameDemoStats.plays}</h3> */}
         </div>
-        <div style={{ position: "absolute", zIndex: "1", right: "0", margin: "2vh"}}>
+        <div
+          style={{
+            position: "absolute",
+            zIndex: "1",
+            right: "0",
+            margin: "2vh"
+          }}
+        >
           <button className="btn btn-outline-success">Buy food</button>
         </div>
         <Toy
